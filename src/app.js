1e4861ba675b71f75cfd7949b552675b89f87d41
Borja <borjaalvarez_18@hotmail.com>
2023-03-18 15:06:28 +0100

    drop comment in test

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-//initial route
 app.get("/", (_req, res) => {
   res.send("Ey muy buenas a todos guapisimos y guapisimas");
 });
