6564b8f644362a29c0243919f4125d78481ee47b
Borja <borjaalvarez_18@hotmail.com>
2023-03-18 15:04:54 +0100

    test git credentials

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+//initial route
 app.get("/", (_req, res) => {
   res.send("Ey muy buenas a todos guapisimos y guapisimas");
 });
