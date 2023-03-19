141d65a95617bac337ae71b00293e481209ea7c7
Borja <borja.alvarez@hubbusiness.design>
2023-03-19 11:15:26 +0100

    testing

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,8 @@
-import express, { Express, Request, Response } from "express";
-import cors from "cors";
+import app from "./app";
 import * as dotenv from "dotenv";
-
 dotenv.config({ path: "config/.env" });
 
-const app: Express = express();
-app.use(cors({ origin: true }));
-
 const port = process.env.PORT;
-
-app.get("/", (_req: Request, res: Response) => {
-  res.json({ t: "Ey muy buenas a todos guapisimos y guapisimas" });
-});
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
