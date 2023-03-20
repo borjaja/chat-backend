9bd820b6e6dd747a098e01e2c8e0c0e053abda76
Borja <borja.alvarez@hubbusiness.design>
2023-03-20 19:46:37 +0100

    testing in backend

--- src/app/app.test.ts
+++ src/app/app.test.ts
@@ -1,9 +1,16 @@
-const request = require("supertest");
-const app = require("./app");
+import request from "supertest";
+import app from "./app";
 
 describe("Test the root path", () => {
   test("It should response the GET method", async () => {
     const response = await request(app).get("/");
+    expect(response.statusCode).toBe(200);
+  });
+  test("It should response the GET method", async () => {
+    const response = await request(app)
+      .post("/auth/sign-up")
+      .set("Content-type", "application/json")
+      .send({ username: "user" });
     expect(response.statusCode).toBe(200);
   });
 });
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,12 @@
 import express, { Express, Request, Response } from "express";
 import middlewares from "./middlewares";
+import { MongoClient } from "mongodb";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: "config/.env" });
+
+const url = process.env.ATLAS_URI ?? "";
+const collection = process.env.COLLECTION ?? "";
 
 const app: Express = express();
 
@@ -10,8 +17,12 @@ app.get("/", (_req: Request, res: Response) => {
   res.json({ t: "Ey muy buenas a todos guapisimos y guapisimas" });
 });
 
-app.post("/auth/sign-up", (req: Request, res: Response) => {
-  console.log(req.body);
+app.post("/auth/sign-up", async (req: Request, res: Response) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const db = await MongoClient.connect(url);
+  console.log(db);
 
   res.json({ t: "Ey muy buenas a todos guapisimos y guapisimas" });
 });
